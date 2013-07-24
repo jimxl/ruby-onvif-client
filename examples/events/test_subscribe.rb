4ad2d71af1a6ab4b83db3677450009a104f866b4
lkw <liaokw@cnjabsco.com>
2013-07-24 17:32:39 +0800

    api:subscribe

---
+++ examples/events/test_subscribe.rb
@@ -0,0 +1,17 @@
+require_relative "../../lib/ruby_onvif_client"
+
+EM.run do 
+    event = ONVIF::Event.new("http://192.168.16.106/onvif/event_service")
+    event.get_event_service_address ->(address) {
+        puts "address=",address
+        content = {}
+        content[:address] = 'http://192.168.16.197/onvif_notify_server'
+        content[:initial_termination_time] = 'PT10S'
+        event.subscribe content, ->(success, result) {
+            if success
+                puts "ooooooooooooook", result
+            end
+        }      
+    }    
+   
+end
--- lib/ruby_onvif_client/event_handing/subscribe.rb
+++ lib/ruby_onvif_client/event_handing/subscribe.rb
@@ -1,26 +1,21 @@
 require_relative '../action'
 
 module ONVIF
-    module EventHandingAction
+    module EventAction
         class Subscribe < Action
-            def run cb
-                message = Message.new
+            def run options, cb
+                message = Message.new namespaces: {:'xmlns:wsdl' => 'http://www.onvif.org/ver10/events/wsdl', :'xmlns:a' => "http://www.w3.org/2005/08/addressing", 'xmlns' => "http://docs.oasis-open.org/wsn/b-2"}
                 message.body =  ->(xml) do
-                    xml.wsdl(:Subscribe)
+                    xml.Subscribe do
+                        xml.wsdl(:ConsumerReference) do
+                            xml.a :Address, options[:address]
+                        end
+                        xml.wsdl :InitialTerminationTime , options[:initial_termination_time]
+                    end
                 end
                 send_message message do |success, result|
                     if success
-                        xml_doc = Nokogiri::XML(result[:content])
-                        users = []
-                        xml_doc.xpath('//tds:User').each do |node|
-                            users << {
-                                name: value(node, 'tt:Username'),
-                                password: value(node, 'tt:Password'),
-                                user_level: value(node, 'tt:UserLevel')
-                            }
-                        end
-
-                        callback cb, success, users
+                        callback cb, success, result
                     else
                         callback cb, success, result
                     end
@@ -30,3 +25,9 @@ module ONVIF
     end
 end
 
+ # <Subscribe xmlns="http://docs.oasis-open.org/wsn/b-2">
+ #      <ConsumerReference>
+ #        <a:Address>http://192.168.16.197/onvif_notify_server</a:Address>
+ #      </ConsumerReference>
+ #      <InitialTerminationTime>PT10S</InitialTerminationTime>
+ #    </Subscribe>
