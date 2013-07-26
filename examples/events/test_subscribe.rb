6f00d6989f19a8875f070d7bb2a4f016ac0c0b0b
lkw <liaokw@cnjabsco.com>
2013-07-26 10:54:28 +0800

    subscribe

--- examples/events/test_subscribe.rb
+++ examples/events/test_subscribe.rb
@@ -1,11 +1,12 @@
 require_relative "../../lib/ruby_onvif_client"
-
-EM.run do 
+require_relative "../../lib/ruby_onvif_client/server.rb"
+EM.run do
+	EM::start_server("0.0.0.0", 8080, ONVIF::Server)
     event = ONVIF::Event.new("http://192.168.16.106/onvif/event_service")
     event.get_event_service_address ->(address) {
         puts "address=",address
         content = {}
-        content[:address] = 'http://192.168.16.197/onvif_notify_server'
+        content[:address] = 'http://192.168.16.251:8080/onvif_notify_server'
         content[:initial_termination_time] = 'PT10S'
         event.subscribe content, ->(success, result) {
             if success
--- lib/ruby_onvif_client/event_handing/subscribe.rb
+++ lib/ruby_onvif_client/event_handing/subscribe.rb
@@ -4,18 +4,31 @@ module ONVIF
     module EventAction
         class Subscribe < Action
             def run options, cb
-                message = Message.new namespaces: {:'xmlns:wsdl' => 'http://www.onvif.org/ver10/events/wsdl', :'xmlns:a' => "http://www.w3.org/2005/08/addressing", 'xmlns' => "http://docs.oasis-open.org/wsn/b-2"}
+                message = Message.new namespaces: {:'xmlns:wsdl' => 'http://www.onvif.org/ver10/events/wsdl', :'xmlns:a' => "http://www.w3.org/2005/08/addressing", 'xmlns:b' => "http://docs.oasis-open.org/wsn/b-2"}
                 message.body =  ->(xml) do
-                    xml.Subscribe do
-                        xml.wsdl(:ConsumerReference) do
+                    xml.b(:Subscribe) do
+                        xml.b(:ConsumerReference) do
                             xml.a :Address, options[:address]
                         end
-                        xml.wsdl :InitialTerminationTime , options[:initial_termination_time]
+                        xml.b :InitialTerminationTime , options[:initial_termination_time]
                     end
                 end
                 send_message message do |success, result|
                     if success
-                        callback cb, success, result
+                        xml_doc = Nokogiri::XML(result[:content])
+                        addresses = []
+                        xml_doc.xpath('//tev:SubscriptionReference').each do |node|
+                            addresses << value(node, 'wsa5:Address')
+                        end                        
+                        res = {}
+                        puts addresses
+                        res[:addresses] = addresses
+                        current_time = xml_doc.xpath('//wsnt:CurrentTime').first.content
+                        termination_time = xml_doc.xpath('//wsnt:TerminationTime').first.content
+                        puts current_time, termination_time
+                        res[:current_time] = current_time
+                        res[:termination_time] = termination_time
+                        callback cb, success, res
                     else
                         callback cb, success, result
                     end
