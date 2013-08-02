82f3d904e424b2868c0331cd4434c45925e9e17d
“fengwz” <fengwz@cnjabsco.com>
2013-08-02 14:36:11 +0800

    修改set_network_interfaces参数传入结构，执行后返回结果结构

--- lib/ruby_onvif_client/device_management/set_network_interfaces.rb
+++ lib/ruby_onvif_client/device_management/set_network_interfaces.rb
@@ -5,31 +5,31 @@ module ONVIF
         class SetNetworkInterfaces < Action
             # network_interface 的结构
             # {
-            #   interface_token: 'xxxxxx',  // name [string]
+            #   interface_token: 'xxxxxx',  // optional name [string]
             #   nwif:{ // #NetworkInterface 
-            #       enabled: true,   //true, false  [boolean]
-            #       link: {
+            #       enabled: true,   //optional  true, false  [boolean]
+            #       link: {//optional
             #           auto_negotiation: true,  //true, false [boolean]
             #           speed: 3,        // [int]
             #           duplex: 'Full'   //'Full', 'Half' 
             #       }
-            #       mtu: 2,   // [int]
-            #       ipv4: {
-            #           enabled: true, //true, false [boolean]
-            #           manual: [{
+            #       mtu: 2,   //optional  [int]
+            #       ipv4: { //optional
+            #           enabled: true, //optional true, false [boolean]
+            #           manual: [{ //optional
             #               address: "xxx.xxx.xxx.xxx", // [IPv4Address]
             #               prefix_length: 22 // [int]
             #           }],
-            #           dhcp: true //true, false [boolean]
+            #           dhcp: true //optional true, false [boolean]
             #       },
-            #       ipv6: {
-            #           enabled: true, //true, false [boolean]
-            #           ara: false//true, false  #AcceptRouterAdvert    [boolean]
-            #           manual: [{
+            #       ipv6: {//optional
+            #           enabled: true, //optional  true, false [boolean]
+            #           ara: false//optional  true, false  #AcceptRouterAdvert    [boolean]
+            #           manual: [{//optional
             #               address: "xxx.xxx.xxx.xxx", // [IPv4Address]
             #               prefix_length: 22 // [int]
             #           }],
-            #           dhcp: true //true, false [boolean]
+            #           dhcp: true //optional  true, false [boolean]
             #       }
             #   }
             # }
@@ -39,43 +39,74 @@ module ONVIF
                     xml.wsdl(:SetNetworkInterfaces) do
                         xml.wsdl :InterfaceToken, network_interface[:interface_token]
                         xml.wsdl :NetworkInterface do
-                            xml.sch :Enabled, network_interface[:nwif][:enabled]
-                            xml.sch :Link do
-                                xml.sch :AutoNegotiation, network_interface[:nwif][:link][:auto_negotiation]
-                                xml.sch :Speed, network_interface[:nwif][:link][:speed]
-                                xml.sch :Duplex, network_interface[:nwif][:link][:duplex]
+                            unless options[:nwif][:enabled].nil?
+                                xml.sch :Enabled, network_interface[:nwif][:enabled]
                             end
-                            xml.sch :MTU, network_interface[:nwif][:mtu]
-                            xml.sch :IPv4 do
-                                xml.sch :Enabled, network_interface[:nwif][:ipv4][:enabled]
-                                network_interface[:nwif][:ipv4][:manual].each do |manual|
-                                    xml.sch :Manual do
-                                        xml.sch :Address, manual[:address]
-                                        xml.sch :PrefixLength, manual[:prefix_length]
+                            unless options[:nwif][:link].nil?
+                                xml.sch :Link do
+                                    xml.sch :AutoNegotiation, network_interface[:nwif][:link][:auto_negotiation]
+                                    xml.sch :Speed, network_interface[:nwif][:link][:speed]
+                                    xml.sch :Duplex, network_interface[:nwif][:link][:duplex]
+                                end
+                            end
+                            unless options[:nwif][:mtu].nil?
+                                xml.sch :MTU, network_interface[:nwif][:mtu]
+                            end
+                            unless options[:nwif][:ipv4].nil?
+                                xml.sch :IPv4 do
+                                    unless options[:nwif][:ipv4][:enabled].nil?
+                                        xml.sch :Enabled, network_interface[:nwif][:ipv4][:enabled]
+                                    end
+                                    unless options[:nwif][:ipv4][:manual].nil?
+                                        network_interface[:nwif][:ipv4][:manual].each do |manual|
+                                            xml.sch :Manual do
+                                                xml.sch :Address, manual[:address]
+                                                xml.sch :PrefixLength, manual[:prefix_length]
+                                            end
+                                        end
+                                    end
+                                    unless options[:nwif][:ipv4][:dhcp].nil?
+                                        xml.sch :DHCP, network_interface[:nwif][:ipv4][:dhcp]
                                     end
                                 end
-                                xml.sch :DHCP, network_interface[:nwif][:ipv4][:dhcp]
                             end
-                            xml.sch :IPv6 do
-                                xml.sch :Enabled, network_interface[:nwif][:ipv6][:enabled]
-                                xml.sch :AcceptRouterAdvert, network_interface[:nwif][:ipv6][:ara]
-                                network_interface[:nwif][:ipv6][:manual].each do |manual|
-                                    xml.sch :Manual do
-                                        xml.sch :Address, manual[:address]
-                                        xml.sch :PrefixLength, manual[:prefix_length]
+                            unless options[:nwif][:ipv6].nil?
+                                xml.sch :IPv6 do
+                                    unless options[:nwif][:ipv6][:enabled].nil?
+                                        xml.sch :Enabled, network_interface[:nwif][:ipv6][:enabled]
+                                    end
+                                    unless options[:nwif][:ipv6][:ara].nil?
+                                        xml.sch :AcceptRouterAdvert, network_interface[:nwif][:ipv6][:ara]
+                                    end
+                                    unless options[:nwif][:ipv6][:manual].nil?
+                                        network_interface[:nwif][:ipv6][:manual].each do |manual|
+                                            xml.sch :Manual do
+                                                xml.sch :Address, manual[:address]
+                                                xml.sch :PrefixLength, manual[:prefix_length]
+                                            end
+                                        end
+                                    end
+                                    unless options[:nwif][:ipv6][:dhcp].nil?
+                                        xml.sch :DHCP, network_interface[:nwif][:ipv6][:dhcp]
                                     end
                                 end
-                                xml.sch :DHCP, network_interface[:nwif][:ipv6][:dhcp]
                             end
-
                         end
                         xml.wsdl :Extension do
                         end
                     end
                 end
                 send_message message do |success, result|
-                    #????
-                    callback cb, success, result
+                    if success
+                        xml_doc = Nokogiri::XML(result[:content])
+                        info = {
+                            reboot_needed: value(xml_doc, '//tds:RebootNeeded')
+                        }
+
+                        callback cb, success, info
+                    else
+                        callback cb, success, result
+                    end
                 end
             end
         end
